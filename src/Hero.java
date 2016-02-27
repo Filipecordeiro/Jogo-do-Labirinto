17cd5f9e8d065a6af98b1ee13dc46e6f2ac0fc80
teresaguilherme <up201305016@fe.up.pt>
2016-02-27 20:24:11 +0000

    Update Hero.java

--- src/Hero.java
+++ src/Hero.java
@@ -1,7 +1,16 @@
 
 public class Hero extends Movable
 {
+	protected boolean state;
 	public Hero(int[] p){
 		super(p);
+		state=false;	
+	}
+	public void setState()
+	{
+		state =true;
+	}
+	public boolean getState(){
+		return state;
 	}
 }
